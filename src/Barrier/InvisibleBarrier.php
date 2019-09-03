7a5cf33e721d8bc3108fa02dabbf7d8e10ab367b
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-09-03 16:55:42 -0700

    fixed error

--- src/Barrier/InvisibleBarrier.php
+++ src/Barrier/InvisibleBarrier.php
@@ -1,6 +1,6 @@
 <?php
 
-public function onMove(PlayerMoveEvent $event){
+function onMove(PlayerMoveEvent $event){
 if($this->isInside($event->getPlayer()->getPosition(), $pos1, $pos2)){
 $event->getPlayer()->teleport($event->getFrom());
 // Send message if you want to inform players why they're knocked back
